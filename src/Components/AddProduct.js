36d33332c828567967456b9747175b9185c20f63
markand <markandyuvasoft408@gmail.com>
2023-02-04 19:06:28 +0530

    ad

--- src/Components/AddProduct.js
+++ src/Components/AddProduct.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import './AddProduct.css'
 import netflixLogo from '../../src/assets/images/netflixLogo.jpg';
 
